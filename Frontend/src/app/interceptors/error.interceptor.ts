99db871c4859f97da790957ac6aebf6d62c270bc
mateimitnei <matei.mitnei@stud.acs.upb.ro>
2026-08-11 12:02:15 +0300

    Server connection error message

--- Frontend/src/app/interceptors/error.interceptor.ts
+++ Frontend/src/app/interceptors/error.interceptor.ts
@@ -4,7 +4,12 @@ import { catchError, throwError } from 'rxjs';
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
     return next(req).pipe(
         catchError((error) => {
-            const errorMessage = error.error?.detail || error.error?.title || `${error.status} ${error.statusText}`;
+            let errorMessage: string;
+            if (error.status === 0) {
+                errorMessage = "Cannot connect to the server.";
+            } else {
+                errorMessage = error.error?.detail || error.error?.title || `${error.status} ${error.statusText}`;
+            }
             alert(`Error: ${errorMessage}`);
             return throwError(() => error);
         })
